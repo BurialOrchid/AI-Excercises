d2339b5986d37719442eadbe2a42dfad10c1f86f
Krzysztof <krzysiek.mroziewicz@gmail.com>
2020-01-07 21:32:40 +0100

    Still problems with MinMax in Exercise 7

--- Exercise07/MainWindow.xaml.cs
+++ Exercise07/MainWindow.xaml.cs
@@ -3,119 +3,190 @@ using System.Windows.Controls;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Threading.Tasks;
+using System;
 
 namespace Exercise07
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    ///
+
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             PointsAI = 0;
             PointsHuman = 0;
-
+            Board = new char[3, 3] { { 'a', 'a', 'a' }, { 'a', 'a', 'a' }, { 'a', 'a', 'a' } };
+            Turn = true;
             InitializeComponent();
-            DataContext = this;
-            sth();
         }
 
         public int PointsAI { get; set; }
         public int PointsHuman { get; set; }
         public bool Turn { get; set; }
+        public char[,] Board { get; set; }
 
-
-        private void sth()
+        private void PlayersTurn(Button button)
         {
-          
+            button.Content = 'X';
+            switch (button.Name)
+            {
+                case "Button1": Board[0, 0] = 'X'; break;
+                case "Button2": Board[0, 1] = 'X'; break;
+                case "Button3": Board[0, 2] = 'X'; break;
+                case "Button4": Board[1, 0] = 'X'; break;
+                case "Button5": Board[1, 1] = 'X'; break;
+                case "Button6": Board[1, 2] = 'X'; break;
+                case "Button7": Board[2, 0] = 'X'; break;
+                case "Button8": Board[2, 1] = 'X'; break;
+                case "Button9": Board[2, 2] = 'X'; break;
+                default: break;
+            }
+            DisableButtons();
+            DrawBoard();
         }
-        private int CheckIfWinning(char player)
+
+        private void DrawBoard()
         {
-            char[,] board = new char[3, 3] { { 'a', 'a', 'a' }, { 'a', 'a', 'a' }, { 'a', 'a', 'a' } };
-            if (Button1.Content != null)
-                board[0, 0] = (char)Button1.Content;
-            if (Button2.Content != null)
-                board[0, 1] = (char)Button2.Content;
-            if (Button3.Content != null)
-                board[0, 2] = (char)Button3.Content;
-            if (Button4.Content != null)
-                board[1, 0] = (char)Button4.Content;
-            if (Button5.Content != null)
-                board[1, 1] = (char)Button5.Content;
-            if (Button6.Content != null)
-                board[1, 2] = (char)Button6.Content;
-            if (Button7.Content != null)
-                board[2, 0] = (char)Button7.Content;
-            if (Button8.Content != null)
-                board[2, 1] = (char)Button8.Content;
-            if (Button9.Content != null)
-                board[2, 2] = (char)Button9.Content;
-
-            //check rows
-            if (board[0, 0] == player && board[0, 1] == player && board[0, 2] == player) { return 1; }
-            if (board[1, 0] == player && board[1, 1] == player && board[1, 2] == player) { return 1; }
-            if (board[2, 0] == player && board[2, 1] == player && board[2, 2] == player) { return 1; }
-
-            // check columns
-            if (board[0, 0] == player && board[1, 0] == player && board[2, 0] == player) { return 1; }
-            if (board[0, 1] == player && board[1, 1] == player && board[2, 1] == player) { return 1; }
-            if (board[0, 2] == player && board[1, 2] == player && board[2, 2] == player) { return 1; }
-
-            // check diags
-            if (board[0, 0] == player && board[1, 1] == player && board[2, 2] == player) { return 1; }
-            if (board[0, 2] == player && board[1, 1] == player && board[2, 0] == player) { return 1; }
             for (int i = 0; i < 3; i++)
             {
                 for (int j = 0; j < 3; j++)
                 {
-                    if (board[j, i] == 'a') return -1;
+                    Debug.Write(Board[i, j]);
+                }
+                Debug.WriteLine("");
+            }
+
+            Debug.WriteLine("");
+        }
+
+        private void AIsTurn()
+        {
+            DoBestMove();
+        }
+
+        private void DoBestMove()
+        {
+        }
+
+        private int CheckIfWinning(char[,] board)
+        {
+            //check rows
+            if (board[0, 0] == board[0, 1] && board[0, 1] == board[0, 2])
+            {
+                if (board[0, 0] == 'X') return 1;
+                if (board[0, 0] == 'O') return -1;
+            }
+
+            if (board[1, 0] == board[1, 1] && board[1, 1] == board[1, 2])
+            {
+                if (board[1, 0] == 'X') return 1;
+                if (board[1, 0] == 'O') return -1;
+            }
+            if (board[2, 0] == board[2, 1] && board[2, 1] == board[2, 2])
+            {
+                if (board[2, 0] == 'X') return 1;
+                if (board[2, 0] == 'O') return -1;
+            }
+
+            // check columns
+            if (board[0, 0] == board[1, 0] && board[1, 0] == board[2, 0])
+            {
+                if (board[0, 0] == 'X') return 1;
+                if (board[0, 0] == 'O') return -1;
+            }
+            if (board[0, 1] == board[1, 1] && board[1, 1] == board[2, 1])
+            {
+                if (board[0, 1] == 'X') return 1;
+                if (board[0, 1] == 'O') return -1;
+            }
+            if (board[0, 2] == Board[1, 2] && board[1, 2] == board[2, 2])
+            {
+                if (board[0, 2] == 'X') return 1;
+                if (board[0, 2] == 'O') return -1;
+            }
+
+            // check diags
+            if (board[0, 0] == board[1, 1] && board[1, 1] == board[2, 2])
+            {
+                if (board[0, 0] == 'X') return 1;
+                if (board[0, 0] == 'O') return -1;
+            }
+            if (board[0, 2] == board[1, 1] && board[1, 1] == board[2, 0])
+            {
+                if (board[0, 2] == 'X') return 1;
+                if (board[0, 2] == 'O') return -1;
+            }
+
+            return 0;
+        }
+
+        private bool IsMovesLeft(char[,] board)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (board[i, j] == 'a')
+                        return true;
                 }
             }
-            return 0;
+            return false;
+        }
 
+        private void DisableButtons()
+        {
+            foreach (Button item in PlayGrid.Children)
+            {
+                if (item.Content != null)
+                {
+                    item.IsEnabled = false;
+                }
+            }
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-
-            Turn = !Turn;
-            PlayerNameLabel.Content = Turn ? "AI's turn" : "Player's turn";
             Button button = sender as Button;
-            switch (Turn)
+            PlayersTurn(button);
+            AIsTurn();
+
+            switch (CheckIfWinning(Board))
             {
-
-                case true:
-                    button.Content = 'X';
-                    button.IsEnabled = false;
-                    if (CheckIfWinning('X') == 1)
+                case 0:
+                    if (!IsMovesLeft(Board))
                     {
-                        this.PointsHuman++;
-                        MessageBox.Show($"HUMAN WON", "GAME OVER");
-                        foreach (Button item in PlayGrid.Children)
-                        {
-                            item.IsEnabled = false;
-                        }
-                    };
-
-                    if (CheckIfWinning('X') == 0) MessageBox.Show($"TIE", "GAME OVER");
-
-                    break;
-
-                case false:
-                    button.Content = 'O';
-                    button.IsEnabled = false;
-                    if (CheckIfWinning('O') == 1)
-                    {
-                        this.PointsAI++;
-                        MessageBox.Show($"AI WON", "GAME OVER");
+                        MessageBox.Show($"TIE", "GAME OVER");
                         foreach (Button item in PlayGrid.Children)
                         {
                             item.IsEnabled = false;
                         }
                     }
-                    if (CheckIfWinning('O') == 0) MessageBox.Show($"TIE", "GAME OVER");
+                    break;
 
+                case 1:
+                    foreach (Button item in PlayGrid.Children)
+                    {
+                        item.IsEnabled = false;
+                    }
+                    PointsHuman++;
+                    MessageBox.Show($"PLAYER WON", "GAME OVER");
+                    this.HumanScoreValue.Content = PointsHuman;
+                    break;
+
+                case -1:
+                    foreach (Button item in PlayGrid.Children)
+                    {
+                        item.IsEnabled = false;
+                    }
+                    PointsAI++;
+                    MessageBox.Show($"AI WON", "GAME OVER");
+                    this.AIScoreValue.Content = PointsAI;
+                    break;
+
+                default:
                     break;
             }
         }
@@ -127,6 +198,8 @@ namespace Exercise07
                 button.IsEnabled = true;
                 button.Content = null;
             }
+            Board = new char[3, 3] { { 'a', 'a', 'a' }, { 'a', 'a', 'a' }, { 'a', 'a', 'a' } };
+            DrawBoard();
         }
     }
 }
